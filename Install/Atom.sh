9cbc395dc8a31ac2c446e81322d6f350accd0e60
Edgar Toll <edjopato@gmail.com>
2017-01-26 16:13:52 +0100

    Improve readability

--- Install/Atom.sh
+++ Install/Atom.sh
@@ -1,32 +1,46 @@
 # apm upgrade --no-confirm
+
+### general
 apm install editorconfig
+
 ### minimap
 apm install minimap minimap-git-diff minimap-find-and-replace minimap-selection
+
 ### file icons
 apm install file-icons
+
 ### formating
 apm install atom-beautify sort-lines
+
 ### linter
 apm install linter linter-mixed-indent minimap-linter
+
 ### languages
 apm install language-lua language-vhdl language-svg
+
 ### c
 apm install switch-header-source
 apm install linter-gcc
+
 ### webdev
 apm install autoclose-html
 apm install linter-tslint language-typescript-grammars-only
 apm install linter-jshint linter-jsonlint linter-sass-lint
+
 ### latex
 apm install language-latex latex
 apm install pdf-view
 apm install linter-chktex
+
 ### highlight selected
 apm install highlight-selected minimap-highlight-selected
+
 ### git blame
 apm install git-blame
+
 ### split diff / git time machine
 apm install git-time-machine split-diff minimap-split-diff
+
 ### color viewer
 # apm install pigments
 apm install color-picker
