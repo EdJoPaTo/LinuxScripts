1668278a606d16de4c65b8b64ab093f42def557a
Edgar Toll <edjopato@gmail.com>
2017-06-27 16:49:57 +0200

    feat: dont need that scripts anymore

--- Arch/scripts/day-skin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.gedit.preferences.editor scheme 'classic'
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc'
--- Arch/scripts/night-skin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
-gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
