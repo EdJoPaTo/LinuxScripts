9a3b354d56c57930e872592accd6849d88c129b3
Edgar Toll <edjopato@gmail.com>
2017-11-30 18:02:04 +0100

    feat: add bigger sizes

--- Applications/NautilusScripts/SvgToAppIcon.sh
+++ Applications/NautilusScripts/SvgToAppIcon.sh
@@ -8,7 +8,7 @@ do
 
   mkdir appicon
 
-  for size in 16 32 57 60 72 76 114 120 144 152 180 192 512
+  for size in 16 32 57 60 72 76 114 120 144 152 180 192 512 1000 1024
   do
     inkscape -z -e "appicon/$size.png" -w $size -h $size "$filename" >/dev/null 2>/dev/null
   done
