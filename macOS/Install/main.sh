318d8f66043614565389808a210a81afa19ed001
Edgar Toll <edjopato@gmail.com>
2019-01-28 20:59:14 +0100

    feat: as this is a linux script repo add some macOS brew scripts
    😬

---
+++ macOS/Install/main.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Console tools
+brew install exa ffmpeg hugo  zsh
+
+# Image stuff
+brew install imagemagick
+brew cask install gimp inkscape
+
+# Media
+brew cask install iina vlc
+
+# Data
+brew install rsync
+brew cask install resilio-sync
+
+# Communication
+brew cask install telegram telegram-desktop teamspeak-client mumble discord slack
+
+# Development Tools
+brew install bat cloc mtr nmap node
+brew cask install dash atom visual-studio-code wireshark
+
+# gpg stuff
+brew install pinentry pinentry-mac
+
+# Development Virtualisation
+brew cask install docker virtualbox
+
+# Development Boards
+brew cask install etcher ftdi-vcp-driver silicon-labs-vcp-driver wch-ch34x-usb-serial-driver
+
+# Small tools
+brew cask install keepingyouawake quitter veracrypt
+
+# Office
+brew cask install mactex libreoffice
---
+++ macOS/Install/quick-look.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# See https://github.com/sindresorhus/quick-look-plugins
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json webpquicklook suspicious-package quicklookase qlvideo
