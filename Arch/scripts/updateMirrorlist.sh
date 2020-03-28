785dfde2e895116f1e2f78f30c7ce853e268204c
Edgar Toll <edjopato@gmail.com>
2020-03-28 17:22:56 +0100

    feat(arch): more piping and more verbose update mirrorlist

--- Arch/scripts/updateMirrorlist.sh
+++ Arch/scripts/updateMirrorlist.sh
@@ -1,14 +1,16 @@
-echo "Download Mirrorlist..."
-wget -q -O /tmp/mirrorlist "https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6"
-sed -i 's/^#Server/Server/' /tmp/mirrorlist
-echo "Rank Mirrors..."
-rankmirrors -n 6 /tmp/mirrorlist > /tmp/mirrorlist.new
-cat /tmp/mirrorlist.new
-rm /tmp/mirrorlist
+#!/bin/sh -e
+
+echo "Download Mirrorlist and rank them..."
+curl --silent "https://www.archlinux.org/mirrorlist/?country=DE&protocol=https&ip_version=4&ip_version=6&use_mirror_status=on" \
+	| sed 's/^#Server/Server/' \
+	| sed '/^#/ d' \
+	| rankmirrors -v -n 6 - | tee /dev/tty \
+	| sed '/^#/ d' \
+	> /tmp/mirrorlist
 echo "Backup old mirrorlist"
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo "Set new mirrorlist"
-sudo mv /tmp/mirrorlist.new /etc/pacman.d/mirrorlist
+sudo mv /tmp/mirrorlist /etc/pacman.d/mirrorlist
 
 echo "Download Updates with new Mirrorlist"
-sudo pacman -Syyuw --noconfirm --noprogressbar
+sudo pacman -Syyuw --noconfirm
