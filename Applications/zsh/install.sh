d2fe1796f3ccda1893ef33a5e16231d9e157c459
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-04-23 00:36:34 +0200

    feat(zsh): go back to link seperate files
    easier extendable

--- Applications/zsh/install.sh
+++ Applications/zsh/install.sh
@@ -2,8 +2,10 @@
 mkdir -p "$HOME/.zsh"
 mkdir -p "$HOME/.cache"
 
-mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
-cat zshrc > "$HOME/.zshrc"
-cat *.zsh >> "$HOME/.zshrc"
+# verbose relative symbolic force
+lnoptions="-vrsf"
+
+ln $lnoptions zshrc "$HOME/.zshrc"
+ln $lnoptions *.zsh "$HOME/.zsh"
 
 git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
