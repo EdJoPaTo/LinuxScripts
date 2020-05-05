79c164201bc87e016d036312855974c66bb3e5f8
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-05-05 15:09:33 +0200

    feat(zsh): add aliases for nodejs and youtube-dl

--- Applications/zsh/aliases.zsh
+++ Applications/zsh/aliases.zsh
@@ -22,3 +22,9 @@ alias ls="exa"
 
 alias fd='find . -type d -name'
 alias ff='find . -type f -name'
+
+alias youtube-dl-mp3='nice youtube-dl --write-all-thumbnails --add-metadata --embed-thumbnail -k -x --audio-format mp3'
+
+alias init-nvm='source /usr/share/nvm/init-nvm.sh'
+alias npm-reinstall='rm -rf node_modules package-lock.json && nice npm i && npm outdated'
+alias typescript-watch='rm -rf dist && npx tsc --sourceMap --pretty -w'
