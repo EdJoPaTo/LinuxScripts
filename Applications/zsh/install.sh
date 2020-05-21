bac527d7fa93f9fed7eccdbeb85bcef42e67619f
EdJoPaTo <edjopato@gmail.com>
2020-05-21 11:23:03 +0200

    refactor(zsh): simplify install for macOS and Arch
    same script for both with way less adaptations needed

--- Applications/zsh/install.sh
+++ Applications/zsh/install.sh
@@ -1,12 +1,15 @@
-#!/bin/sh
+#!/bin/zsh
 mkdir -p "$HOME/.zsh"
 mkdir -p "$HOME/.cache"
 
 # verbose relative symbolic force
+# remove r on macOS
 lnoptions="-vrsf"
 
-ln $lnoptions zshrc "$HOME/.zshrc"
-ln $lnoptions *.zsh "$HOME/.zsh"
-# cp -v *.zsh "$HOME/.zsh"
+ln $lnoptions "$(pwd)/zshrc" "$HOME/.zshrc"
+
+for config_file ($(pwd)/*.zsh); do
+    ln $lnoptions $config_file "$HOME/.zsh"
+done
 
 git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
