d255b17af475cf18d80ad075eb1ecd7f8f0830da
EdJoPaTo <edjopato@gmail.com>
2020-05-21 11:24:27 +0200

    feat(zsh): add macos-specific stuff

---
+++ Applications/zsh/macos-specific.zsh
@@ -0,0 +1,9 @@
+# macOS specific
+
+alias androidstudio="open -a /Applications/Android\ Studio.app"
+
+#export JAVA_8_HOME=$(/usr/libexec/java_home -v1.8)
+export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
+
+#alias java8='export JAVA_HOME=$JAVA_8_HOME'
+alias java11='export JAVA_HOME=$JAVA_11_HOME'
