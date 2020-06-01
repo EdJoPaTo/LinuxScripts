3913b06d2ab44e8fcdf0f11ba82e481403765b80
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-01 03:00:03 +0200

    feat(zsh): add ros noetic

--- Applications/zsh/archlinux-specific.zsh
+++ Applications/zsh/archlinux-specific.zsh
@@ -5,3 +5,5 @@
 export MAKEFLAGS="-j$(nproc)"
 
 alias setup-ros-melodic='source /opt/ros/melodic/setup.zsh'
+alias setup-ros-noetic='source /opt/ros/noetic/setup.zsh'
+alias setup-ros='setup-ros-melodic'
