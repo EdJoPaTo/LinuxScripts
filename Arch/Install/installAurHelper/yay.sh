fb6fbdabee24b8b932663581044fd71610174e4b
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-06-18 11:36:51 +0200

    feat(Arch): never show diff menu for yay

--- Arch/Install/installAurHelper/yay.sh
+++ Arch/Install/installAurHelper/yay.sh
@@ -7,4 +7,4 @@ param="--noconfirm --needed -S"
 
 ./installaurpackage.sh yay-bin
 
-yay --save --batchinstall --combinedupgrade --noremovemake
+yay --save --batchinstall --combinedupgrade --noremovemake --nodiffmenu
