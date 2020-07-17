782ba7cf8a1e1378519fe185d85f89213c678f26
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-07-18 01:00:14 +0200

    feat(mpd): add mpd config

---
+++ Applications/mpd/install.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+mkdir -p "$HOME/.config/mpd"
+mkdir -p "$HOME/.cache/mpd"
+# Assume the default english xdg music folder (https://wiki.archlinux.org/index.php/XDG_user_directories)
+mkdir -p "$HOME/Music"
+
+# verbose relative symbolic force
+lnoptions="-vrsf"
+
+ln $lnoptions "$(pwd)/mpd.conf" "$HOME/.config/mpd/mpd.conf"
+
+systemctl enable --user --now mpd.service
---
+++ Applications/mpd/readme.md
@@ -0,0 +1,7 @@
+Setup mpd for the current user which then runs as long as the user is logged in.
+
+You can then use mpc from the command line to add and play/pause things like that:
+
+Assuming `~/Music/party.mp3` exists: `mpc add party.mp3`
+
+`mpc play`
