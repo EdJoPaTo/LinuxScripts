b4abab39cf79fe56d8b3f05b4f499aa572cd3d6a
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-07-23 10:24:09 +0200

    feat(zsh): add alias for open
    macos has open as a default which is handy sometimes

--- Applications/zsh/archlinux-specific.zsh
+++ Applications/zsh/archlinux-specific.zsh
@@ -1,5 +1,7 @@
 # Arch Linux Specific
 
+alias open='xdg-open'
+
 # make AUR packages with all processor cores
 # https://wiki.archlinux.org/index.php/Makepkg#Parallel_compilation
 export MAKEFLAGS="-j$(nproc)"
