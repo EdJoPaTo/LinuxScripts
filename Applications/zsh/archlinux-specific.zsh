93ba9e2bc11094cb6d101173ad477b2e0225811d
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-08-09 12:05:32 +0200

    feat(zsh): nice yay

--- Applications/zsh/archlinux-specific.zsh
+++ Applications/zsh/archlinux-specific.zsh
@@ -6,6 +6,8 @@ alias open='xdg-open'
 # https://wiki.archlinux.org/index.php/Makepkg#Parallel_compilation
 export MAKEFLAGS="-j$(nproc)"
 
+alias yay='nice yay'
+
 alias setup-ros-melodic='source /opt/ros/melodic/setup.zsh'
 alias setup-ros-noetic='source /opt/ros/noetic/setup.zsh'
 alias setup-ros='setup-ros-melodic'
