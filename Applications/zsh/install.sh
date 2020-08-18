53557b4c3f7a83214165cfe55fce4ecc4d187cd0
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-08-18 13:12:13 +0200

    fix(zsh): clone theme before config
    dependency before main
    config uses theme so the theme should be there first

--- Applications/zsh/install.sh
+++ Applications/zsh/install.sh
@@ -4,6 +4,8 @@ set -e
 mkdir -p "$HOME/.zsh"
 mkdir -p "$HOME/.cache"
 
+git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+
 # verbose relative symbolic force
 # remove r on macOS
 lnoptions="-vrsf"
@@ -14,4 +16,3 @@ for config_file ($(pwd)/*.zsh); do
     ln $lnoptions $config_file "$HOME/.zsh"
 done
 
-git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
