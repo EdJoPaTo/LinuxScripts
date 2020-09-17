2d891f1413e1c76a46595870b240f8397b399c99
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-09-17 10:15:20 +0200

    fix(zsh): dont abort install when theme exists

--- Applications/zsh/install.sh
+++ Applications/zsh/install.sh
@@ -4,7 +4,7 @@ set -e
 mkdir -p "$HOME/.zsh"
 mkdir -p "$HOME/.cache"
 
-git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+git clone --depth=1 https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure" || true
 
 # verbose relative symbolic force
 # remove r on macOS
@@ -15,4 +15,3 @@ ln $lnoptions "$(pwd)/zshrc" "$HOME/.zshrc"
 for config_file ($(pwd)/*.zsh); do
     ln $lnoptions $config_file "$HOME/.zsh"
 done
-
