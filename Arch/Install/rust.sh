d5c25d5de36f585885bb3f32fc58dff27b7ab70d
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2020-10-14 09:59:25 +0200

    fix(Arch): VSCode extension rust-analyzer is better than rust

--- Arch/Install/rust.sh
+++ Arch/Install/rust.sh
@@ -9,6 +9,3 @@ sudo pacman $param rustup
 
 rustup default stable
 rustup toolchain install stable
-
-# Rust Language Server needed in Visual Studio Code
-rustup component add rls rust-analysis rust-src
