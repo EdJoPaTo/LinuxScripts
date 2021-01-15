b51637153cf38f83b4340ee4a6ab4874bbcf687e
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-01-15 10:05:48 +0100

    refactor(Arch): cleanup aur helper installation

--- Arch/Install/installAurHelper/paru.sh
+++ Arch/Install/installAurHelper/paru.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/sh
 set -e
 
-sudo pacman -Sy
-
-param="--noconfirm --needed -S"
-
 # Switch to paru-bin and remove make dep when available for arm64
 # https://github.com/Morganamilo/paru/issues/83
-sudo pacman $param cargo
+sudo pacman --noconfirm --needed -Sy cargo
 
 ./installaurpackage.sh paru
 
--- Arch/Install/installAurHelper/yay.sh
+++ Arch/Install/installAurHelper/yay.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/sh
 set -e
 
-sudo pacman -Sy
-
-param="--noconfirm --needed -S"
-
 ./installaurpackage.sh yay-bin
 
 yay --gendb
