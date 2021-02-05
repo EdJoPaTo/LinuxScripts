0f79653b470fb7d0e6f80e5bc1c2bcbd0cfab895
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-02-05 02:18:56 +0100

    feat(Arch): automatically enable extensions

--- Arch/Install/installWithAurHelper/gnome.sh
+++ Arch/Install/installWithAurHelper/gnome.sh
@@ -5,6 +5,7 @@ EXPLICIT=(
     gnome-shell-extension-caffeine-git
     gnome-shell-extension-sound-output-device-chooser
     gnome-shell-extension-system-monitor-git
+    gnome-shell-extensions # drive-menu and launch-new-instance
     nautilus-open-any-terminal
 )
 
@@ -15,8 +16,14 @@ sudo pacman -Sy
 # nautilus-open-any-terminal
 gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
 
+EXTENSIONS=(
+    caffeine@patapon.info
+    drive-menu@gnome-shell-extensions.gcampax.github.com
+    launch-new-instance@gnome-shell-extensions.gcampax.github.com
+    sound-output-device-chooser@kgshank.net
+    system-monitor@paradoxxx.zero.gmail.com
+)
 
-echo
-echo "install extensions manually:"
-echo https://extensions.gnome.org/extension/600/launch-new-instance/
-echo https://extensions.gnome.org/extension/7/removable-drive-menu/
+for extension in "${EXTENSIONS[@]}"; do
+    gnome-extensions enable $extension
+done
