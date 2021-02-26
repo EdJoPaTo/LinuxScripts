51f72c694a2f9c711ae0ed5f05593fa3ac346a91
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-02-26 01:37:57 +0100

    perf(Arch): switch from paru to paru-bin

--- Arch/Install/installAurHelper/paru.sh
+++ Arch/Install/installAurHelper/paru.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/sh
 set -e
 
-# Switch to paru-bin and remove make dep when available for arm64
-# https://github.com/Morganamilo/paru/issues/83
-sudo pacman --noconfirm --needed -Sy cargo
-
-./installaurpackage.sh paru
+./installaurpackage.sh paru-bin
 
 paru --gendb
