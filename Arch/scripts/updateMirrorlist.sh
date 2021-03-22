04eda50b1c0167dfee1df58b75d6fd96594be4fb
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-03-22 14:01:56 +0100

    fix(Arch): prevent older mirrors

--- Arch/scripts/updateMirrorlist.sh
+++ Arch/scripts/updateMirrorlist.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Download Mirrorlist and rank them..."
-reflector --latest 100 --protocol https --ipv4 --ipv6 --country de,be,fr,nl --connection-timeout 1 --sort rate > /tmp/mirrorlist
+reflector --latest 20 --protocol https --ipv4 --ipv6 --country de,be,fr,nl --connection-timeout 1 --sort rate > /tmp/mirrorlist
 echo "Backup old mirrorlist"
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo "Set new mirrorlist"
