c0a24515c9bc3681cdcc78aab2bf041cb8333ce9
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-05-21 09:06:09 +0200

    refactor(configs): move mpd to configs

--- Applications/random-configs/install-linux.sh
+++ Applications/random-configs/install-linux.sh
@@ -12,3 +12,6 @@ link "$(pwd)/dive.yaml" "$HOME/.config/dive/dive.yaml"
 link "$(pwd)/htoprc" "$HOME/.config/htop/htoprc"
 link "$(pwd)/paru.conf" "$HOME/.config/paru/paru.conf"
 link "$(pwd)/vscode-settings.json" "$HOME/.config/Code - OSS/User/settings.json"
+
+mkdir -p "$HOME/.cache/mpd"
+link "$(pwd)/mpd.conf" "$HOME/.config/mpd/mpd.conf"
