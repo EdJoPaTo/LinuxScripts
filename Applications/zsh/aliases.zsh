1652ee90f55b6919762da5e432d0fef9bf9467f9
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-11-03 09:57:38 +0100

    fix(aliases): rsync can create target folder itself now

--- Applications/zsh/aliases.zsh
+++ Applications/zsh/aliases.zsh
@@ -95,6 +95,6 @@ remotedebug() {
 	folder=${PWD##*/}
 	remotefolder="tmp/remotedebug/$folder/"
 
-	rsync --archive --compress --verbose --checksum --delete-delay --exclude=.git --exclude-from=.gitignore --rsync-path="mkdir -p $remotefolder && rsync" . $server:$remotefolder
+	rsync --archive --compress --verbose --checksum --delete-delay --exclude=.git --exclude-from=.gitignore . $server:$remotefolder
 	ssh -tt $server "uptime && cd $remotefolder && bash -cl '$@' && uptime"
 }
