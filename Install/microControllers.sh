ad2970904559b0cc8bac4b742be00755700465c2
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-11-23 23:16:30 +0100

    feat(microcontrollers): simplify manual work afterwards

--- Install/microControllers.sh
+++ Install/microControllers.sh
@@ -4,7 +4,21 @@ set -e
 mkdir -p ~/.platformio/lib
 mkdir -p ~/Arduino/libraries/Credentials
 
-echo ensure 3 \#define are set in ~/Arduino/libraries/Credentials/credentials.h
-echo WIFI_SSID WIFI_PASSWORD MQTT_SERVER
+file="$HOME/Arduino/libraries/Credentials/credentials.h"
+
+insert() {
+    rg -q "$1" "$file" 2> /dev/null || echo "$1$2" >> "$file"
+}
+
+insert "#pragma once"
+
+insert "#define WIFI_SSID " '""'
+insert "#define WIFI_PASSWORD " '""'
+
+insert "#define MQTT_SERVER " '""'
+insert "#define MQTT_USERNAME " 'NULL'
+insert "#define MQTT_PASSWORD " 'NULL'
 
 ln -srf ~/Arduino/libraries/Credentials ~/.platformio/lib
+
+echo "ensure all #define are correct in $file"
