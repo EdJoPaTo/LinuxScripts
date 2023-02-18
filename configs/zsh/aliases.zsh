21a2c11ce1f7b318ab3866dfad4c5fcbd9b14dde
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-02-18 14:08:42 +0100

    feat(aliases): more clippy lints
    these are probably too much and will be reduced over time

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -92,7 +92,34 @@ alias cargo-open-doc='nice cargo doc --open --all-features'
 alias cargoBelow='project-below --file=Cargo.toml nice -n 19 cargo'
 cargo-pedantic() {
 	nice cargo fmt -- --check
-	nice cargo clippy --no-deps --all-targets "$@" -- -W clippy::pedantic -W clippy::nursery
+	# See lints: https://rust-lang.github.io/rust-clippy/stable/index.html
+	# Restriction lints are listed individually
+	nice cargo clippy --no-deps --all-targets "$@" -- \
+		-W clippy::alloc_instead_of_core \
+		-W clippy::as_underscore \
+		-W clippy::create_dir \
+		-W clippy::decimal_literal_representation \
+		-W clippy::empty_drop \
+		-W clippy::empty_structs_with_brackets \
+		-W clippy::filetype_is_file \
+		-W clippy::float_cmp_const \
+		-W clippy::fn_to_numeric_cast_any \
+		-W clippy::format_push_string \
+		-W clippy::if_then_some_else_none \
+		-W clippy::let_underscore_must_use \
+		-W clippy::lossy_float_literal \
+		-W clippy::mixed_read_write_in_expression \
+		-W clippy::multiple_inherent_impl \
+		-W clippy::panic \
+		-W clippy::partial_pub_fields \
+		-W clippy::rest_pat_in_fully_bound_structs \
+		-W clippy::same_name_method \
+		-W clippy::self_named_module_files \
+		-W clippy::std_instead_of_core \
+		-W clippy::unnecessary_self_imports \
+		-W clippy::unwrap_in_result \
+		-W clippy::nursery \
+		-W clippy::pedantic
 	nice cargo build --all-targets "$@" &&
 		nice cargo test --quiet "$@"
 }
@@ -104,7 +131,37 @@ cargo-clippy-tests() {
 	nice cargo fmt -- --check
 	nice cargo test --all-features
 	nice cargo doc --all-features --no-deps
-	nice cargo clippy --all-features --all-targets -- -W clippy::pedantic -W clippy::nursery -W clippy::cargo
+	nice cargo clippy --all-features --all-targets -- \
+		-W clippy::alloc_instead_of_core \
+		-W clippy::as_underscore \
+		-W clippy::create_dir \
+		-W clippy::decimal_literal_representation \
+		-W clippy::empty_drop \
+		-W clippy::empty_structs_with_brackets \
+		-W clippy::filetype_is_file \
+		-W clippy::float_cmp_const \
+		-W clippy::fn_to_numeric_cast_any \
+		-W clippy::format_push_string \
+		-W clippy::if_then_some_else_none \
+		-W clippy::indexing_slicing \
+		-W clippy::let_underscore_must_use \
+		-W clippy::lossy_float_literal \
+		-W clippy::mixed_read_write_in_expression \
+		-W clippy::multiple_inherent_impl \
+		-W clippy::panic \
+		-W clippy::partial_pub_fields \
+		-W clippy::rest_pat_in_fully_bound_structs \
+		-W clippy::same_name_method \
+		-W clippy::self_named_module_files \
+		-W clippy::std_instead_of_alloc \
+		-W clippy::std_instead_of_core \
+		-W clippy::str_to_string \
+		-W clippy::unnecessary_self_imports \
+		-W clippy::unwrap_in_result \
+		-W clippy::unwrap_used \
+		-W clippy::cargo \
+		-W clippy::nursery \
+		-W clippy::pedantic
 }
 
 cat /usr/local/opt/nvm/nvm.sh &>/dev/null && alias nvm='unalias nvm && source /usr/local/opt/nvm/nvm.sh && nvm'
