bfbcff892b82fb927c4cdade7d7d62074c0e744f
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-05-01 18:12:32 +0200

    refactor: migrate Deno.run to Command

--- Arch/Install/analyze-packages.ts
+++ Arch/Install/analyze-packages.ts
@@ -1,28 +1,28 @@
 #!/usr/bin/env -S deno run --allow-run=pacman
 
-async function exec(cmd: [string, ...string[]]): Promise<string> {
-  const process = Deno.run({ cmd, stdout: "piped" });
-  const status = await process.status();
+export async function exec(cmd: string, ...args: string[]): Promise<string> {
+  const process = new Deno.Command(cmd, { args, stdout: "piped" }).spawn();
+  const status = await process.status;
   if (!status.success) {
-    throw new Error(`Command ${cmd[0]} was not successful`);
+    throw new Error(`Command ${cmd} was not successful`);
   }
 
-  const outputBuffer = await process.output();
+  const { stdout: outputBuffer } = await process.output();
   const output = new TextDecoder().decode(outputBuffer);
   return output;
 }
 
-const allExplicitsRaw = await exec(["pacman", "-Qeq"]);
+const allExplicitsRaw = await exec("pacman", "-Qeq");
 const allExplicits = allExplicitsRaw.split("\n").filter(Boolean);
 console.log("allExplicits", allExplicits.length);
 
-const allDependenciesRaw = await exec(["pacman", "-Qdq"]);
+const allDependenciesRaw = await exec("pacman", "-Qdq");
 const allDependencies = allDependenciesRaw.split("\n").filter(Boolean);
 console.log("allDependencies", allDependencies.length);
 
 const onlyOptionalsUnfiltered = await Promise.all(
   allDependencies.map(async (p) => {
-    const output = await exec(["pacman", "-Qi", p]);
+    const output = await exec("pacman", "-Qi", p);
     const isRequired = !output.includes("Required By     : None");
     return isRequired ? "" : p;
   }),
@@ -34,7 +34,7 @@ console.log()
 
 const unfiltered = await Promise.all(
   allExplicits.map(async (p) => {
-    const output = await exec(["pacman", "-Qi", p]);
+    const output = await exec("pacman", "-Qi", p);
     const isRequired = !output.includes("Required By     : None");
     return isRequired ? p : "";
   }),
