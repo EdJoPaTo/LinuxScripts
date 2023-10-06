f4f7ecfe7bdc4019cb792d4e44ce1c89906e4324
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-10-06 11:39:31 +0200

    style: use tabs

--- Arch/Install/analyze-packages.ts
+++ Arch/Install/analyze-packages.ts
@@ -1,15 +1,15 @@
 #!/usr/bin/env -S deno run --allow-run=pacman
 
 export async function exec(cmd: string, ...args: string[]): Promise<string> {
-  const process = new Deno.Command(cmd, { args, stdout: "piped" }).spawn();
-  const status = await process.status;
-  if (!status.success) {
-    throw new Error(`Command ${cmd} was not successful`);
-  }
+	const process = new Deno.Command(cmd, { args, stdout: "piped" }).spawn();
+	const status = await process.status;
+	if (!status.success) {
+		throw new Error(`Command ${cmd} was not successful`);
+	}
 
-  const { stdout: outputBuffer } = await process.output();
-  const output = new TextDecoder().decode(outputBuffer);
-  return output;
+	const { stdout: outputBuffer } = await process.output();
+	const output = new TextDecoder().decode(outputBuffer);
+	return output;
 }
 
 const allExplicitsRaw = await exec("pacman", "-Qeq");
@@ -21,24 +21,28 @@ const allDependencies = allDependenciesRaw.split("\n").filter(Boolean);
 console.log("allDependencies", allDependencies.length);
 
 const onlyOptionalsUnfiltered = await Promise.all(
-  allDependencies.map(async (p) => {
-    const output = await exec("pacman", "-Qi", p);
-    const isRequired = !output.includes("Required By     : None");
-    return isRequired ? "" : p;
-  }),
+	allDependencies.map(async (p) => {
+		const output = await exec("pacman", "-Qi", p);
+		const isRequired = !output.includes("Required By     : None");
+		return isRequired ? "" : p;
+	}),
 );
 const onlyOptionals = onlyOptionalsUnfiltered.filter(Boolean);
 
 console.log("optional", onlyOptionals.length, onlyOptionals.join(" "));
-console.log()
+console.log();
 
 const unfiltered = await Promise.all(
-  allExplicits.map(async (p) => {
-    const output = await exec("pacman", "-Qi", p);
-    const isRequired = !output.includes("Required By     : None");
-    return isRequired ? p : "";
-  }),
+	allExplicits.map(async (p) => {
+		const output = await exec("pacman", "-Qi", p);
+		const isRequired = !output.includes("Required By     : None");
+		return isRequired ? p : "";
+	}),
 );
 const onlyExplicitDependencies = unfiltered.filter(Boolean);
 
-console.log("explicit&required", onlyExplicitDependencies.length, onlyExplicitDependencies.join(" "));
+console.log(
+	"explicit&required",
+	onlyExplicitDependencies.length,
+	onlyExplicitDependencies.join(" "),
+);
