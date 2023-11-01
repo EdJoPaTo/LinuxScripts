bb0656c47fd9c2257db8997c72b1461b3473509d
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-11-01 11:21:22 +0100

    fix(Arch): ssh on sway

--- configs/zsh/archlinux-specific.zsh
+++ configs/zsh/archlinux-specific.zsh
@@ -5,7 +5,8 @@ if [[ $OSTYPE = linux* ]]; then
 # https://wiki.archlinux.org/index.php/Makepkg#Parallel_compilation
 export MAKEFLAGS="-j$(nproc)"
 
-# fix ssh key stuff with gnome-keyring
-export SSH_AUTH_SOCK=/run/user/$(id --user)/keyring/ssh
+# fix ssh key stuff with gnome-keyring on sway
+# requires systemctl --user enable --now gcr-ssh-agent.service
+export SSH_AUTH_SOCK=/run/user/$(id --user)/gcr/ssh
 
 fi
