3896955879d63249a6cc7db70336ad0e77ea947a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-11-28 10:40:43 +0100

    refactor(configs): simplify link function usage

--- configs/install.sh
+++ configs/install.sh
@@ -6,14 +6,14 @@ function link() {
 	rm -rf "$2"
 	if [[ $OSTYPE = darwin* ]]; then
 		# verbose symbolic force
-		ln -vsf "$1" "$2"
+		ln -vsf "$(realpath "$1")" "$2"
 	else
 		ln --verbose --relative --symbolic --force "$1" "$2"
 	fi
 }
 
 # Bin files
-for file in "$(pwd)"/bin/*; do
+for file in bin/*; do
 	filename=$(basename "$file")
 	link "$file" "$HOME/bin/$filename"
 done
@@ -23,53 +23,53 @@ mkdir -p ~/git/hub/EdJoPaTo/public
 
 # Music Player Daemon
 mkdir -p "$HOME/.cache/mpd/"
-link "$(pwd)/mpd.conf" "$HOME/.config/mpd/mpd.conf"
+link "mpd.conf" "$HOME/.config/mpd/mpd.conf"
 
 # Neovim
-link "$(pwd)/neovim/init.lua" "$HOME/.config/nvim/init.lua"
-link "$(pwd)/neovim/lua" "$HOME/.config/nvim/lua"
+link "neovim/init.lua" "$HOME/.config/nvim/init.lua"
+link "neovim/lua" "$HOME/.config/nvim/lua"
 
 # Linux only
 if [[ $OSTYPE = linux* ]]; then
-	link "$(pwd)/envvars.conf" "$HOME/.config/environment.d/envvars.conf"
-	link "$(pwd)/fuzzel.ini" "$HOME/.config/fuzzel/fuzzel.ini"
-	link "$(pwd)/hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
-	link "$(pwd)/i3status-rust.toml" "$HOME/.config/i3status-rust/config.toml"
-	link "$(pwd)/kanshi" "$HOME/.config/kanshi/config"
-	link "$(pwd)/paru.conf" "$HOME/.config/paru/paru.conf"
-	link "$(pwd)/rofi" "$HOME/.config/rofi"
-	link "$(pwd)/sway" "$HOME/.config/sway/config"
-	link "$(pwd)/swaylock" "$HOME/.config/swaylock/config"
-	link "$(pwd)/waybar" "$HOME/.config/waybar"
+	link "envvars.conf" "$HOME/.config/environment.d/envvars.conf"
+	link "fuzzel.ini" "$HOME/.config/fuzzel/fuzzel.ini"
+	link "hyprland.conf" "$HOME/.config/hypr/hyprland.conf"
+	link "i3status-rust.toml" "$HOME/.config/i3status-rust/config.toml"
+	link "kanshi" "$HOME/.config/kanshi/config"
+	link "paru.conf" "$HOME/.config/paru/paru.conf"
+	link "rofi" "$HOME/.config/rofi"
+	link "sway" "$HOME/.config/sway/config"
+	link "swaylock" "$HOME/.config/swaylock/config"
+	link "waybar" "$HOME/.config/waybar"
 fi
 
 # Zsh
 mkdir -p "$HOME/.cache/"
 mkdir -p "$HOME/.config/completions/"
-link "$(pwd)/zsh/zshrc" "$HOME/.zshrc"
-for file in "$(pwd)"/zsh/*.zsh; do
+link "zsh/zshrc" "$HOME/.zshrc"
+for file in zsh/*.zsh; do
 	filename=$(basename "$file")
 	link "$file" "$HOME/.config/zsh/$filename"
 done
 
-link "$(pwd)/.editorconfig" "$HOME/.editorconfig"
-link "$(pwd)/.tokeignore" "$HOME/.tokeignore"
-link "$(pwd)/alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
-link "$(pwd)/aliases" "$HOME/.config/aliases"
-link "$(pwd)/config.fish" "$HOME/.config/fish/config.fish"
-link "$(pwd)/dive.yaml" "$HOME/.config/dive/dive.yaml"
-link "$(pwd)/env" "$HOME/.config/env"
-link "$(pwd)/gitconfig" "$HOME/.config/git/config"
-link "$(pwd)/global.gitignore" "$HOME/.config/git/global.gitignore"
-link "$(pwd)/helix" "$HOME/.config/helix"
-link "$(pwd)/htoprc" "$HOME/.config/htop/htoprc"
-link "$(pwd)/starship.toml" "$HOME/.config/starship.toml"
-link "$(pwd)/tmux.conf" "$HOME/.config/tmux/tmux.conf"
+link ".editorconfig" "$HOME/.editorconfig"
+link ".tokeignore" "$HOME/.tokeignore"
+link "alacritty.yml" "$HOME/.config/alacritty/alacritty.yml"
+link "aliases" "$HOME/.config/aliases"
+link "config.fish" "$HOME/.config/fish/config.fish"
+link "dive.yaml" "$HOME/.config/dive/dive.yaml"
+link "env" "$HOME/.config/env"
+link "gitconfig" "$HOME/.config/git/config"
+link "global.gitignore" "$HOME/.config/git/global.gitignore"
+link "helix" "$HOME/.config/helix"
+link "htoprc" "$HOME/.config/htop/htoprc"
+link "starship.toml" "$HOME/.config/starship.toml"
+link "tmux.conf" "$HOME/.config/tmux/tmux.conf"
 
 rm -f "$HOME/.gitconfig"
 
 if [[ $OSTYPE = darwin* ]]; then
-	link "$(pwd)/vscode-settings.json" "$HOME/Library/Application Support/VSCodium/User/settings.json"
+	link "vscode-settings.json" "$HOME/Library/Application Support/VSCodium/User/settings.json"
 else
-	link "$(pwd)/vscode-settings.json" "$HOME/.config/Code - OSS/User/settings.json"
+	link "vscode-settings.json" "$HOME/.config/Code - OSS/User/settings.json"
 fi
