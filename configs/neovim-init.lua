30b984c04d65060d6a285cc14d4a0eeece4bd9b2
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-15 18:03:18 +0100

    refactor(neovim): from packer to lazy

--- configs/neovim-init.lua
+++ configs/neovim-init.lua
@@ -15,34 +15,25 @@ vim.keymap.set('n', '<leader>f', '<Cmd>Files<CR>')
 -- clean install with
 -- rm -rf ~/.cache/nvim ~/.local/share/nvim ~/.local/state/nvim ~/.config/nvim/plugin
 
-local ensure_packer = function()
-    local fn = vim.fn
-    local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-    if fn.empty(fn.glob(install_path)) > 0 then
-        fn.system({
-            "git", "clone", "--depth", "1",
-            "https://github.com/wbthomason/packer.nvim", install_path
-        })
-        vim.cmd [[packadd packer.nvim]]
-        return true
-    end
-    return false
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system {
+		'git',
+		'clone',
+		'--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git',
+		'--branch=stable', -- latest stable release
+		lazypath,
+	}
 end
-
-local packer_bootstrap = ensure_packer()
-
-return require("packer").startup(function(use)
-    use {"wbthomason/packer.nvim"}
-
-    use {
-        "nvim-lualine/lualine.nvim",
-        config = function() require("lualine").setup() end
-    } -- Status Line
-    use {"airblade/vim-gitgutter"} -- Git diff in sign column
-    use {"editorconfig/editorconfig-vim"}
-    use {"junegunn/fzf.vim"}
-
-    -- Automatically set up your configuration after cloning packer.nvim
-    -- Put this at the end after all plugins
-    if packer_bootstrap then require("packer").sync() end
-end)
+vim.opt.rtp:prepend(lazypath)
+require('lazy').setup({
+	{ 'nvim-lualine/lualine.nvim', opts = {} },
+	"airblade/vim-gitgutter",
+	{
+		"junegunn/fzf.vim",
+		dependencies = {
+			"junegunn/fzf"
+		}
+	},
+})
