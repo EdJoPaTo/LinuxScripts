4a8e882c8b893ccf156b72965df41037d0bee517
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-02-15 18:38:03 +0100

    feat(neovim): add some default plugins

--- configs/neovim-init.lua
+++ configs/neovim-init.lua
@@ -8,14 +8,16 @@ vim.o.mouse = "a"
 vim.o.number = true
 vim.o.signcolumn = 'yes'
 vim.o.smartcase = true
+vim.o.timeout = true
+vim.o.timeoutlen = 300
 vim.o.undofile = true
 
 vim.g.mapleader = " " -- leader key
 vim.g.maplocalleader = " "
 
 -- fzf to switch files quickly
-vim.keymap.set('n', '<leader>b', '<Cmd>Buffers<CR>')
-vim.keymap.set('n', '<leader>f', '<Cmd>Files<CR>')
+vim.keymap.set('n', '<leader>b', '<Cmd>Buffers<CR>', { desc = "Switch to open buffers quickly" })
+vim.keymap.set('n', '<leader>f', '<Cmd>Files<CR>', { desc = "Open files quickly" })
 
 -- clean install with
 -- rm -rf ~/.cache/nvim ~/.local/share/nvim ~/.local/state/nvim ~/.config/nvim/plugin
@@ -33,12 +35,53 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
-	{ 'nvim-lualine/lualine.nvim', opts = {} },
-	"airblade/vim-gitgutter",
+	{ 'nvim-lualine/lualine.nvim', opts = { theme = 'onedark' } },
+	{ 'lewis6991/gitsigns.nvim', opts = {} },
 	{
 		"junegunn/fzf.vim",
 		dependencies = {
 			"junegunn/fzf"
 		}
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+
+	-- Autocomplete commands
+	{ 'gelguy/wilder.nvim', opts = { modes = { ':', '/', '?' } } },
+
+	-- Useful plugin to show pending keybinds
+	{
+		'folke/which-key.nvim',
+		opts = {
+			plugins = {
+				presets = {
+					operators = true,
+					motions = true,
+					text_objects = true,
+					windows = true,
+					nav = true,
+					z = true,
+					g = true,
+				}
+			}
+		}
+	},
+
+	{
+		-- Theme inspired by Atom
+		'navarasu/onedark.nvim',
+		priority = 1000,
+		lazy = false,
+		config = function()
+			require('onedark').setup({
+				style = 'warmer',
+				transparent = true,
+				lualine = {
+					transparent = true,
+				}
+			})
+			require('onedark').load()
+		end,
 	},
 })
