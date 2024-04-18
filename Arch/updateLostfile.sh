caea27d8ea7370e8b3686d79431ac0621bb328bc
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-04-18 04:55:21 +0200

    feat: adapt to current usage

---
+++ Arch/updateLostfile.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -eu
+
+sudo lostfiles -s |
+	rg -v /etc/ca-certificates/ |
+	rg -v /etc/ssh/ssh_host_ |
+	rg -v /etc/ssl/ |
+	rg -v /usr/share/mime/ |
+	rg -v /var/cache/fontconfig/ |
+	rg -v /var/cache/man/ |
+	rg -v /var/cache/pacman/ |
+	rg -v /var/lib/flatpak |
+	rg -v /var/lib/pacman/local |
+	rg -v /var/lib/pacman/sync |
+	rg -v /var/log/journal/ \
+		>~/tmp/lostfiles.txt
