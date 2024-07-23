8212b0e85d7a9040b7f220091530efabc722074a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2024-07-23 15:01:32 +0200

    feat(rust): install llvm-tools for llvm-cov

--- install/rust.sh
+++ install/rust.sh
@@ -4,6 +4,8 @@ set -eux -o pipefail
 rustup default stable
 rustup toolchain install nightly
 
+rustup component add llvm-tools-preview
+rustup component add llvm-tools-preview --toolchain nightly
 rustup component add rust-analyzer
 
 rustup target add wasm32-unknown-unknown
