9b67223c16deb26cbaf77b333021bffb9bde7f62
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-03-26 10:50:17 +0100

    refactor(zsh): simplify os specific files

--- configs/zsh/archlinux-specific.zsh
+++ configs/zsh/archlinux-specific.zsh
@@ -1,5 +1,5 @@
 # Arch Linux Specific
-if [[ $OSTYPE = linux* ]]; then
+if [[ $OSTYPE != linux* ]]; then; return; fi
 
 # make AUR packages with all processor cores
 # https://wiki.archlinux.org/index.php/Makepkg#Parallel_compilation
@@ -8,5 +8,3 @@ export MAKEFLAGS="-j$(nproc)"
 # fix ssh key stuff with gnome-keyring on sway
 # requires systemctl --user enable --now gcr-ssh-agent.service
 export SSH_AUTH_SOCK=/run/user/$(id --user)/gcr/ssh
-
-fi
--- configs/zsh/macos-specific.zsh
+++ configs/zsh/macos-specific.zsh
@@ -1,5 +1,5 @@
 # macOS specific
-if [[ $OSTYPE = darwin* ]]; then
+if [[ $OSTYPE != darwin* ]]; then; return; fi
 
 alias python=python3
 
@@ -10,5 +10,3 @@ export JAVA_11_HOME=$(/usr/libexec/java_home -v11)
 alias java11='export JAVA_HOME=$JAVA_11_HOME'
 
 podman machine list --format "{{range .}}{{.Name}}\t{{.VMType}}\t{{.LastUp}}\n{{end -}}" | rg running
-
-fi
