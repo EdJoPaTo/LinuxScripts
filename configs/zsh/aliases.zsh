7e5842fb058ac8787c764187eb88fc069bd08000
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-03-26 12:28:06 +0100

    perf(zsh): speedup load time

--- configs/zsh/aliases.zsh
+++ configs/zsh/aliases.zsh
@@ -8,5 +8,5 @@ alias -g ......='../../../../..'
 # ZSH Static named directories (~templates can be used then)
 hash -d templates=~/git/hub/template
 
-cat /usr/local/opt/nvm/nvm.sh &>/dev/null && alias nvm='unalias nvm && source /usr/local/opt/nvm/nvm.sh && nvm'
-cat /usr/share/nvm/init-nvm.sh &>/dev/null && alias nvm='unalias nvm && source /usr/share/nvm/init-nvm.sh && nvm'
+[ -f /usr/local/opt/nvm/nvm.sh ] && alias nvm='unalias nvm && source /usr/local/opt/nvm/nvm.sh && nvm'
+[ -f /usr/share/nvm/init-nvm.sh ] && alias nvm='unalias nvm && source /usr/share/nvm/init-nvm.sh && nvm'
