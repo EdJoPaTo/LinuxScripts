a0ea532448be4ac6d628deed90d66b5401833d0c
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-03-30 09:34:43 +0200

    feat(flatpak): update installed apps

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+# List installed: flatpak list --app
+
 EXPLICIT=(
+	com.github.tenderowl.frog
 	com.mattjakeman.ExtensionManager
+	in.varb.Ausweiskopie
+	io.github.diegoivan.pdf_metadata_editor
 	io.gitlab.adhami3310.Footage
 	org.gnome.Fractal
-	org.jitsi.jitsi-meet
 	org.raspberrypi.rpi-imager
+	# org.jitsi.jitsi-meet
 	# org.signal.Signal
 	# org.telegram.desktop
 )
