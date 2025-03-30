4322dedc6ad0721c88804ee80857b887ac6d0374
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-03-30 12:58:29 +0200

    feat(Arch): improve finding lostfiles

--- Arch/updateLostfile.sh
+++ Arch/updateLostfile.sh
@@ -2,20 +2,36 @@
 set -eu
 
 sudo lostfiles -s |
+	rg -v /\.updated$ |
+	rg -v /texmf |
 	rg -v ^/boot/initramfs |
-	rg -v ^/etc/.updated |
+	rg -v ^/etc/NetworkManager/system-connections/ |
 	rg -v ^/etc/ca-certificates/ |
+	rg -v ^/etc/doas.conf$ |
 	rg -v ^/etc/fonts/conf.d/ |
+	rg -v ^/etc/hostname$ |
+	rg -v ^/etc/locale.conf$ |
+	rg -v ^/etc/machine-id$ |
+	rg -v ^/etc/os-release$ |
+	rg -v ^/etc/pacman.d/gnupg |
 	rg -v ^/etc/ssh/ssh_host_ |
 	rg -v ^/etc/ssl/ |
-	rg -v ^/etc/NetworkManager/system-connections/ |
 	rg -v ^/etc/systemd/.+\.wants |
+	rg -v ^/etc/vconsole.conf$ |
+	rg -v ^/usr/lib/ghc- |
 	rg -v ^/usr/share/mime/ |
 	rg -v ^/var/cache/fontconfig/ |
 	rg -v ^/var/cache/man/ |
 	rg -v ^/var/cache/pacman/ |
+	rg -v ^/var/lib/NetworkManager/ |
 	rg -v ^/var/lib/flatpak |
+	rg -v ^/var/lib/gdm/ |
 	rg -v ^/var/lib/pacman/local |
 	rg -v ^/var/lib/pacman/sync |
-	rg -v ^/var/log/journal/ \
+	rg -v ^/var/lib/systemd/timers/ |
+	rg -v ^/var/log/journal/ |
+	rg -v ^/var/tmp/flatpak-cache- |
+	rg -v ^/var/tmp/systemd-private- \
 		>/tmp/lostfiles.txt
+
+exec less /tmp/lostfiles.txt
