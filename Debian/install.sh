6ac23173fc350c230fb59102ee9b421ba39c9933
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2025-04-20 02:50:55 +0200

    feat(Debian): basic install script for interactive use

---
+++ Debian/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+EXPLICIT=(
+	bat
+	fd-find
+	fish
+	htop
+	neovim
+	ripgrep
+)
+
+apt-get update
+apt-get install -y "${EXPLICIT[@]}"
+
+ln --symbolic --force /usr/bin/batcat /usr/local/bin/bat
+ln --symbolic --force /usr/bin/fdfind /usr/local/bin/fd
