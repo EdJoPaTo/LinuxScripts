a047e859ccc81ac333a61efd251d0427eaebcbf4
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2026-04-14 00:21:33 +0200

    feat: adapt to current usage

--- Arch/init-efi.sh
+++ Arch/init-efi.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env sh
 set -eu
 
+# This script assumes that nvme0n1p1 is /boot and p2 the encrypted /
+#
+# lsblk --output NAME,SIZE,TYPE,MOUNTPOINTS,UUID
+
+uuid=$(blkid --match-tag=UUID --output=value /dev/nvme0n1p2)
+
 efibootmgr \
 	--create \
-	--disk /dev/sda \
+	--disk /dev/nvme0n1 \
 	--part 1 \
 	--label 'Arch Linux' \
 	--loader /vmlinuz-linux \
 	--unicode \
-	'root=/dev/sda2 rw initrd=\initramfs-linux.img'
+	"rd.luks.name=$uuid=root root=/dev/mapper/root rw initrd=\initramfs-linux.img"
